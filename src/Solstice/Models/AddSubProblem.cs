1b6d5570cc1b4da27d75ce6b54fd85f99cd2099f
Deborah Engelmeyer <dengelmeyer@icloud.com>
2016-04-26 12:05:16 -0700

    Create model and controller

---
+++ src/Solstice/Models/AddSubProblem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Solstice.Models
+{
+    public class AddSubProblem
+    {
+        public int AddSubProblemID { get; set; }
+        public int Level { get; set; }
+        public int Operator1 { get; set; }
+        public int Operator2 { get; set; }
+        public int Result { get; set; }
+        public ProblemType ProblemType { get; set; }
+    }
+    public enum ProblemType { Addition, Subtraction, PlaceValue }
+}
