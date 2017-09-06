e7d4efedbfa537fe0d090696c82bac10e3e20f03
Deborah Engelmeyer <dengelmeyer@icloud.com>
2017-09-05 18:37:16 -0700

    Tested feedback page
    Added tests directory and test config

--- mathcookie/config.py
+++ mathcookie/config.py
@@ -8,3 +8,11 @@ class DevConfig(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     RULES_PATH = os.path.join(os.path.realpath(os.path.dirname(__file__)), "static/data")
     # EXPLAIN_TEMPLATE_LOADING = True
+
+class TestConfig(object):
+    SQLALCHEMY_DATABASE_URI = 'postgres://bxveqfofqqielx:4042a3240f3509d83dc13b015b799501422d07ddf4ecd19e81226475ed14c939@ec2-54-225-88-191.compute-1.amazonaws.com:5432/d53ac8b6vgkom5'
+    SECRET_KEY = '8389f90b2b276674cac869412b8a628b'
+    DEBUG = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    RULES_PATH = os.path.join(os.path.realpath(os.path.dirname(__file__)), "static/data")
+    # EXPLAIN_TEMPLATE_LOADING = True
---
+++ mathcookie/tests/test_urls.py
@@ -0,0 +1,9 @@
+"""Test the application routes"""
+import unittest
+
+class TestURLs(unittest.TestCase):
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
