573c094ab207c6e0a06142451fd024a62291c9c8
Deborah Engelmeyer <dengelmeyer@icloud.com>
2017-09-06 14:24:05 -0700

    Start creating tests

--- mathcookie/config.py
+++ mathcookie/config.py
@@ -1,5 +1,6 @@
 """Define configurations for the Math with Cookie app"""
 import os
+import tempfile
 
 class DevConfig(object):
     SQLALCHEMY_DATABASE_URI = 'postgres://bxveqfofqqielx:4042a3240f3509d83dc13b015b799501422d07ddf4ecd19e81226475ed14c939@ec2-54-225-88-191.compute-1.amazonaws.com:5432/d53ac8b6vgkom5'
@@ -10,9 +11,13 @@ class DevConfig(object):
     # EXPLAIN_TEMPLATE_LOADING = True
 
 class TestConfig(object):
-    SQLALCHEMY_DATABASE_URI = 'postgres://bxveqfofqqielx:4042a3240f3509d83dc13b015b799501422d07ddf4ecd19e81226475ed14c939@ec2-54-225-88-191.compute-1.amazonaws.com:5432/d53ac8b6vgkom5'
-    SECRET_KEY = '8389f90b2b276674cac869412b8a628b'
+    db_file = tempfile.NamedTemporaryFile()
     DEBUG = True
+    DEBUG_TB_ENABLED = False
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + db_file.name
+    
+    CACHE_TYPE = 'null'
+    WTF_CSRF_ENABLED = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     RULES_PATH = os.path.join(os.path.realpath(os.path.dirname(__file__)), "static/data")
     # EXPLAIN_TEMPLATE_LOADING = True
--- mathcookie/tests/test_urls.py
+++ mathcookie/tests/test_urls.py
@@ -1,8 +1,29 @@
 """Test the application routes"""
 import unittest
+from .extensions import bcrypt, login_manager, principals, cookie_admin
 
 class TestURLs(unittest.TestCase):
-    pass
+    def setUp(self):
+        # Bug workarounds
+        cookie_admin._views = []
+        
+        app = create_app('mathcookie.config.TestConfig')
+        self.client = app.test_client()
+        
+        # Bug workaround
+        db.app = app
+
+        db.create_all()
+
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+    def test_root_redirect(self):
+        """Tests if the root URL gives a 302"""
+        result = self.client.get('/')
+        assert result.status_code == 302
+        assert 'Cookie' in result.headers['Location']
 
 
 if __name__ == '__main__':
